aa23ade56e0ea047d49f5c25e3347234e94ba9d0
arpright <arpit.chauhan@rightbot.in>
2024-12-22 08:06:08 +0530

    teleop optimisation and cleanup

--- dex_teleop.py
+++ dex_teleop.py
@@ -1,7 +1,6 @@
 import rclpy
 from rclpy.node import Node
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
-from std_msgs.msg import Header
 
 import webcam_teleop_interface as wt
 import simple_ik as si
@@ -16,6 +15,18 @@ from ikpy.chain import Chain
 import urchin as urdf_loader
 import os
 import math
+import time
+from threading import Lock
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time_ms = (end_time - start_time) * 1000  # Convert to milliseconds
+        print(f"Execution time of {func.__name__}: {execution_time_ms:.2f} milliseconds")
+        return result
+    return wrapper
 
 def load_urdf(file_name):
     if not os.path.isfile(file_name):
@@ -41,29 +52,15 @@ class DexTeleopNode(Node):
     def __init__(self):
         super().__init__('dex_teleop_node')
 
-        # Declare and parse parameters
-        self.declare_parameter('use_fastest_mode', False)
-        self.declare_parameter('left_handed', False)
-
-        use_fastest_mode = self.get_parameter('use_fastest_mode').value
-        left_handed = self.get_parameter('left_handed').value
-
+        # Initialize configurations and objects
         self.max_goal_wrist_position_z = dt.goal_max_position_z
         self.min_goal_wrist_position_z = dt.goal_min_position_z
 
-        # Robot speed
-        self.robot_speed = 'fast' if use_fastest_mode else 'slow'
-        self.get_logger().info(f'Running with robot_speed = {self.robot_speed}')
-
-        # Initialize configurations and objects
         self.center_configuration = dt.get_center_configuration()
         self.starting_configuration = dt.get_starting_configuration()
         self.simple_ik = si.SimpleIK()
 
-        if left_handed:
-            self.webcam_aruco_detector = wt.WebcamArucoDetector(tongs_prefix='left', visualize_detections=False)
-        else:
-            self.webcam_aruco_detector = wt.WebcamArucoDetector(tongs_prefix='right', visualize_detections=True)
+        self.webcam_aruco_detector = wt.WebcamArucoDetector(tongs_prefix='right', visualize_detections=False)
 
         # Center wrist position (used for teleoperation origin)
         self.center_wrist_position = self.simple_ik.fk(self.center_configuration)
@@ -72,16 +69,10 @@ class DexTeleopNode(Node):
         # ROS 2 Publishers
         self.trajectory_publisher = self.create_publisher(JointTrajectory, '/arm_controller/joint_trajectory', 10)
         self.gripper_publisher = self.create_publisher(JointTrajectory, '/gripper_controller/joint_trajectory', 10)
-
-        # Marker Pose Publishers (predefined for a maximum of 8 markers)
-        # self.marker_pose_publishers = {
-        #     f'marker_{i}': self.create_publisher(PoseStamped, f'/marker_pose/marker_{i}', 10)
-        #     for i in range(8)
-        # }
         self.goal_pose_publisher = self.create_publisher(PoseStamped, '/goal_pose', 10) 
-        self.origin_publisher = self.create_publisher(PoseStamped, '/origin_pose', 10) 
+
         active_links_mask = [False, True, True, True, True, True, False]
-        urdf_file_name = '/media/rightbot/data/Projects/teleop_tong/giraffe.urdf'
+        urdf_file_name = 'giraffe.urdf'
         self.urdf = load_urdf(urdf_file_name)
         self.giraffe_chain = Chain.from_urdf_file(active_links_mask=active_links_mask, urdf_file=urdf_file_name)
         self.joint_limits = get_joint_limits(self.urdf)
@@ -90,10 +81,12 @@ class DexTeleopNode(Node):
         # Timer
         self.timer_period = 0.1  # 10 Hz
         self.timer = self.create_timer(self.timer_period, self.teleop_callback)
+        self.marker_timer = self.create_timer(self.timer_period, self.marker_callback)
 
         # Debugging
         self.print_timing = False
         self.print_goal = False
+        self.publish_goal_pose_marker = True
         self.loop_timer = lt.LoopTimer()
 
         # Joint names
@@ -104,16 +97,34 @@ class DexTeleopNode(Node):
             'elbow_wrist_1_joint',
             'wrist_1_wrist_2_joint'
         ]
+        self.pose_msg = PoseStamped()
+        self.pose_msg.header.frame_id = "world"
+        self.trajectory_msg = JointTrajectory()
+        self.trajectory_msg.joint_names = self.joint_names
+        self.point = JointTrajectoryPoint()
+        self.point.time_from_start.sec = 1
+        self.gripper_msg = JointTrajectory()
+        self.gripper_msg.joint_names = ['wrist_2_gripper_joint']
+        self.gripper_point = JointTrajectoryPoint()
+        self.gripper_point.time_from_start.sec = 1
 
         # State variables
         self.previous_positions = None  # Track last sent positions to avoid duplicate commands
+        self.markers = None
+        self.marker_lock  = Lock()
 
+
+    def marker_callback(self):
+        with self.marker_lock:
+            self.markers = self.webcam_aruco_detector.process_next_frame()
+
+    # @timer
     def teleop_callback(self):
         self.loop_timer.start_of_iteration()
 
         # Process the next webcam frame
-        markers = self.webcam_aruco_detector.process_next_frame()
-        goal_dict = self.goal_from_markers.get_goal_dict(markers)
+        with self.marker_lock:
+            goal_dict = self.goal_from_markers.get_goal_dict(self.markers)
 
         if goal_dict is not None:
             wrist_position = goal_dict.get('wrist_position', [0.0, 0.0, 0.0])
@@ -135,12 +146,6 @@ class DexTeleopNode(Node):
             gripper_width = goal_dict.get('grip_width', None)
             lower_limit, upper_limit = self.joint_limits['wrist_2_gripper_joint']
             gripper_position = map_within_limits(gripper_width, 0.0, 1.0, lower_limit, upper_limit, 0)
-                
-
-            # Extract orientation axes
-            x_axis = goal_dict.get('gripper_x_axis', [1.0, 0.0, 0.0])
-            y_axis = goal_dict.get('gripper_y_axis', [0.0, 1.0, 0.0])
-            z_axis = goal_dict.get('gripper_z_axis', [0.0, 0.0, 1.0])
 
             # Decompose into roll, pitch, and yaw
             original_rpy = Rotation.from_matrix(rotation_matrix).as_euler('xyz')  # [roll, pitch, yaw]
@@ -148,49 +153,42 @@ class DexTeleopNode(Node):
             # Compute new yaw from wrist position
             new_yaw = -np.arctan2(abs(y),-x )  # Calculate yaw in radians
             new_marker_rpy = [-original_rpy[1], -original_rpy[0], new_yaw]
-            new_marker_rotation_matrix = Rotation.from_euler('xyz', new_marker_rpy).as_matrix()
 
             wrist_1_to_gripper = 0.08
-            wrist_position
-            quaternion = Rotation.from_matrix(new_marker_rotation_matrix).as_quat()  # [x, y, z, w]
+            # wrist_position
             wrist_position[0] = wrist_position[0] * 3 #min(1.0, (3 * 0.36/wrist_position[0]))
             wrist_position[1] = wrist_position[1] * 3 #min(1.0, (3 * 0.36/wrist_position[1]))
             wrist_position[2] = wrist_position[2] + wrist_1_to_gripper * math.sin(new_marker_rpy[1])
 
-            pose_msg = PoseStamped()
-            pose_msg.header.stamp = self.get_clock().now().to_msg()
-            pose_msg.header.frame_id = "world"  # Update as per your RViz frame
+            if self.publish_goal_pose_marker:
+                new_marker_rotation_matrix = Rotation.from_euler('xyz', new_marker_rpy).as_matrix()
+                quaternion = Rotation.from_matrix(new_marker_rotation_matrix).as_quat()  # [x, y, z, w]
 
-            pose_msg.pose.position.x = wrist_position[0]
-            pose_msg.pose.position.y = wrist_position[1]
-            pose_msg.pose.position.z = wrist_position[2]
-            pose_msg.pose.orientation.x = quaternion[0]
-            pose_msg.pose.orientation.y = quaternion[1]
-            pose_msg.pose.orientation.z = quaternion[2]
-            pose_msg.pose.orientation.w = quaternion[3]
+                self.pose_msg.header.stamp = self.get_clock().now().to_msg()
 
-            self.goal_pose_publisher.publish(pose_msg)
+                self.pose_msg.pose.position.x = wrist_position[0]
+                self.pose_msg.pose.position.y = wrist_position[1]
+                self.pose_msg.pose.position.z = wrist_position[2]
+                self.pose_msg.pose.orientation.x = quaternion[0]
+                self.pose_msg.pose.orientation.y = quaternion[1]
+                self.pose_msg.pose.orientation.z = quaternion[2]
+                self.pose_msg.pose.orientation.w = quaternion[3]
+
+                self.goal_pose_publisher.publish(self.pose_msg)
             # Solve IK to get joint positions
             physical_wrist_position = []
             physical_wrist_position.append(wrist_position[1])
             physical_wrist_position.append(-wrist_position[0])
             physical_wrist_position.append(wrist_position[2])
-            physical_wrist_orientation = []
-            physical_wrist_orientation.append(new_marker_rpy[0])
-            physical_wrist_orientation.append(new_marker_rpy[1])
-            physical_wrist_orientation.append(new_marker_rpy[2])
 
-            # joint_positions = self.giraffe_chain.inverse_kinematics(physical_wrist_position, physical_wrist_orientation, orientation_mode="Y")
             joint_positions = self.giraffe_chain.inverse_kinematics(physical_wrist_position)
 
             if len(joint_positions) > 0:
                 # Map joint_positions to the correct order
+                new_marker_rpy[1] = new_marker_rpy[1] + (-3.1415 if new_marker_rpy[1] > 0 else 3.1415)
+
                 ordered_positions = [float(x) for x in joint_positions[1:-1]]
                 ordered_positions[-1] = new_marker_rpy[0] + 1.5708
-                if new_marker_rpy[1] > 0:
-                    new_marker_rpy[1] = new_marker_rpy[1] - 3.1415
-                else:
-                    new_marker_rpy[1] = new_marker_rpy[1] + 3.1415
                 ordered_positions[-2] = ordered_positions[-2] - new_marker_rpy[1]
 
                 # Check if the positions have changed significantly
@@ -198,34 +196,16 @@ class DexTeleopNode(Node):
                     self.get_logger().debug('No significant change in positions; skipping trajectory publish.')
                     return
 
-                # Create a JointTrajectory message
-                trajectory_msg = JointTrajectory()
-                trajectory_msg.header = Header()
-                trajectory_msg.header.stamp.sec = 0
-                trajectory_msg.header.stamp.nanosec = 0
-                trajectory_msg.joint_names = self.joint_names
-
                 # Create a single trajectory point
-                point = JointTrajectoryPoint()
-                point.positions = ordered_positions
-                point.time_from_start.sec = 1
-                point.time_from_start.nanosec = 0
-                trajectory_msg.points.append(point)
+                self.point.positions = ordered_positions
+                self.trajectory_msg.points = [self.point]
+                
+                self.gripper_point.positions = [gripper_position]
+                self.gripper_msg.points = [self.gripper_point]
 
-                gripper_msg = JointTrajectory()
-                gripper_msg.header = Header()
-                gripper_msg.header.stamp.sec = 0
-                gripper_msg.header.stamp.nanosec = 0
-                gripper_msg.joint_names = ['wrist_2_gripper_joint']
-
-                gripper_point = JointTrajectoryPoint()
-                gripper_point.positions = [gripper_position]  # Close the gripper
-                gripper_point.time_from_start.sec = 1 
-                gripper_point.time_from_start.nanosec = 0
-                gripper_msg.points.append(gripper_point)
                 # Publish the trajectory
-                self.trajectory_publisher.publish(trajectory_msg)
-                self.gripper_publisher.publish(gripper_msg)
+                self.trajectory_publisher.publish(self.trajectory_msg)
+                self.gripper_publisher.publish(self.gripper_msg)
 
                 # Update the previous positions
                 self.previous_positions = ordered_positions
