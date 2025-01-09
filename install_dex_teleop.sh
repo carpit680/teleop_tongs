02687cff204c8b9f767515e891c2c69a007dbc3b
carpit680 <carpit680@gmail.com>
2025-01-09 20:27:11 +0530

    cleanup, updated readme and assets

--- install_dex_teleop.sh
+++ install_dex_teleop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Copy udev rule file for the Mi USB Webcam HD."
-echo "sudo cp ./99-hello-dex-teleop-camera.rules /etc/udev/rules.d/"
-sudo cp ./99-hello-dex-teleop-camera.rules /etc/udev/rules.d/
+echo "Copy udev rule file for the Mi USB Webcam."
+echo "sudo cp ./99-teleop-tongs-camera.rules /etc/udev/rules.d/"
+sudo cp ./99-teleop-tongs-camera.rules /etc/udev/rules.d/
 echo ""
 
 echo "Activate the new udev rule."
@@ -15,10 +15,10 @@ echo "sudo apt install v4l-utils"
 sudo apt install v4l-utils
 echo ""
 
-echo "Next, you need to generate specialized URDF files and calibrate your Logitech C930e webcam."
+echo "Next, you need to generate specialized URDF files and calibrate your Mi USB Webcam."
 echo "You can find instructions in the README.md file."
 echo ""
 echo "***********************************************************"
-echo "IMPORTANT: After this installation finishes, you should unplug and then replug your Mi USB Webcam HD, if it's already plugged in."
+echo "IMPORTANT: After this installation finishes, you should unplug and then replug your Mi USB Webcam, if it's already plugged in."
 echo "***********************************************************"
 echo ""
